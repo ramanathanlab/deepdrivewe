5d57258199b0dcf574c3282f42a6a54780c0c8e1
braceal <abrace05@gmail.com>
2024-03-04 16:06:23 -0600

    westpa split merge logic

--- westpa_colmena/apps/pcoord_inference.py
+++ westpa_colmena/apps/pcoord_inference.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 from pathlib import Path
+import itertools
 import numpy as np
 from westpa_colmena.apps.amber_simulation import SimulationResult
-import numpy as np
 from dataclasses import dataclass, field
 
 # TODO: It would be nice to support merge multiple into 1 and split 1 into multiple
@@ -23,102 +23,126 @@ class SimulationMetadata:
             "help": "The ID of the previous simulation the current one is split from, or None if it's a basis state."
         },
     )
+    restart_file : Path | None = field(
+        default=None,
+        metadata={
+            "help": "The restart file for the simulation."
+        },
+    )
+    # TODO: This may not need to be None because the bstates can populate it
+    parent_restart_file: Path | None = field(
+        default=None,
+        metadata={
+            "help": "The restart file for the parent simulation."
+        },
+    )
 
-
-# TODO: Pick up working from here, we know the simulation metadata when
-# splitting, and we can update the simulation result when the simulation returns
-@dataclass
-class SimulationData:
-    simulation: SimulationMetadata
-    result: SimulationResult
+    def __hash__(self) -> int:
+        """Hash the simulation metadata to ensure that it is unique."""
+        return hash((self.simulation_id, self.restart_file))
 
 
 class WeightedEnsemble:
-    data: list[SimulationResult]
-    simulations: list[SimulationMetadata]
+    # TODO: Implement a split-into-multiple and merge-multiple
+    #       merge could take a list of simulations to merge and
+    #       split can take an integer parameter to specify the number of splits
+    # TODO: Figure out a checkpointing mechanism for the metadata
 
-    def __init__(self, basis_states: list[SimulationResult]) -> None:
-        self.data = basis_states
+    # The list of simulations for each iteration
+    simulations: list[list[SimulationMetadata]]
+
+    # The current iteration of the weighted ensemble
+    iteration_idx: int
+
+    def __init__(self, basis_states: list[Path], ensemble_members: int) -> None:
+        # The current iteration of the weighted ensemble
+        self.iteration_idx = 0
+        # The list of simulations for each iteration
+        self.simulations = []
         # Assign a uniform weight to each of the basis states
-        n_states = len(basis_states)
-        weight = 1.0 / n_states
-        self.simulations = [
+        weight = 1.0 / ensemble_members
+
+        # Create a generator that will cycle through the basis states (e.g.,
+        # if ensemble_members is 3 and there are 2 basis states, the generator
+        # will yield [0, basis_state1], [1, basis_state2], [2, basis_state1])
+        sim_generator = zip(range(ensemble_members), itertools.cycle(basis_states))
+
+        # Create the metadata for each basis state
+        sims = [
             SimulationMetadata(
-                weight=weight, simulation_id=idx, prev_simulation_id=None
+                weight=weight,
+                simulation_id=idx,
+                prev_simulation_id=None,
+                restart_file=None,
+                parent_restart_file=basis_state,
             )
-            for idx in range(n_states)
+            for idx, basis_state in sim_generator
         ]
 
-    def get_parent_restart(self, sim: SimulationMetadata) -> Path | None:
-        """Get the restart file for the parent simulation."""
-        if sim.prev_simulation_id is None:
-            return None
-        return self.data[sim.prev_simulation_id].restart_file
+        self.simulations.append(sims)
 
-    # TODO: There is a bug since we currently don't have a way to get the
-    # simulation result for the new simulation
-    def split(self, sim: SimulationMetadata, result: SimulationResult) -> None:
+    @property
+    def current_iteration(self) -> list[SimulationMetadata]:
+        """Return the simulations for the current iteration."""
+        return self.simulations[self.iteration_idx]
+
+    def _add_new_simulation(self, sim: SimulationMetadata, weight: float) -> None:
+        """Add a new simulation to the current iteration."""
+        new_simulation = SimulationMetadata(
+                weight=weight,
+                simulation_id=len(self.current_iteration),
+                prev_simulation_id=sim.simulation_id,
+                restart_file=None,
+                parent_restart_file=sim.restart_file
+        )
+        self.current_iteration.append(new_simulation)
+
+    def advance_iteration(
+            self,
+            to_split: list[SimulationMetadata],
+            to_merge: list[list[SimulationMetadata]]
+    ) -> None:
+        """Advance the iteration of the weighted ensemble."""
+        # Create a list to store the new simulations for this iteration
+        self.simulations.append([])
+        self.iteration_idx += 1
+
+        # Split the simulations
+        for sim in to_split:
+            self.split(sim)
+
+        # Merge the simulations
+        for sims in to_merge:
+            self.merge(sims)
+
+        # Collect any simulations from the previous iteration that were not split or merged
+        sims_to_continue = set(self.simulations[self.iteration_idx - 1])
+        sims_to_continue -= set(to_split)
+        sims_to_continue -= set(itertools.chain(*to_merge))
+
+        # Add the simulations to the current iteration
+        for sim in sims_to_continue:
+            self._add_new_simulation(sim, sim.weight)
+
+    def split(self, sim: SimulationMetadata, n_split: int = 2) -> None:
         """Split the parent simulation, `sim`, into two simulations where
         `result` is the data from the new simulation."""
 
-        # Create a new metadata object to represent the new simulation info
-        new_simulation = SimulationMetadata(
-            weight=sim.weight / 2,
-            simulation_id=len(self.simulations),
-            prev_simulation_id=sim.simulation_id,
-        )
+        # Add the new simulations to the current iteration
+        for _ in range(n_split):
+            self._add_new_simulation(sim, sim.weight / n_split)
 
-        # TODO: Probably easier to store the restart_file and parent_restart_file
-        # in the SimulationMetadata object so that we don't have to store everything
-        # (pcoords, coords, etc) in the SimulationResult object. This means we also
-        # don't need to maintain a separate list of SimulationResult objects which will
-        # make it easier to checkpoint the WeightedEnsemble object, since it will only
-        # store be plain old data and otherwise be stateless. We could store it as a json
-        # file or something similar once per iteration.
+    def merge(self, sims: list[SimulationMetadata]) -> None:
+        """Merge multiple simulations into one."""
 
-        # Update the weight of the parent simulation so that the total
-        # weight of the ensemble sums to one
-        self.simulations[sim.simulation_id].weight /= 2
+        # Get the weights of each simulation to merge
+        weights = [sim.weight for sim in sims]
 
-        # Setup a new simulation result object for the new simulation
-        assert result.restart_file is not None
-        new_result = SimulationResult(
-            pcoord=[],
-            coords=[],
-            restart_file=None,
-            parent_restart_file=result.restart_file
-        )
+        # Randomly select one of the simulations weighted by their weights
+        select: int = np.random.choice(len(sims), p=weights)
 
-        # Add the new simulation data to the lists
-        self.data.append(new_result)
-        self.simulations.append(new_simulation)
-
-    def merge(
-        self,
-        sim1: SimulationMetadata,
-        sim2: SimulationMetadata,
-        result1: SimulationResult,
-        result2: SimulationResult,
-    ) -> None:
-        """Merge two simulations into one."""
-
-        # TODO: Consider making dataclass to collect the simulation data and metadata
-
-        # Randomly select one of the two simulations weighted by their weights
-        select = np.random.choice([0, 1], p=[sim1.weight, sim2.weight])
-        sim = [sim1, sim2][select]
-        result = [result1, result2][select]
-
-        # Create a new metadata object to represent the new simulation info
-        new_simulation = SimulationMetadata(
-            weight=sim1.weight + sim2.weight,
-            simulation_id=len(self.simulations),
-            prev_simulation_id=sim.simulation_id,
-        )
-
-        # Add the new simulation data to the lists
-        self.data.append(result)
-        self.simulations.append(new_simulation)
+        # Add the new simulation to the current iteration
+        self._add_new_simulation(sims[select], sum(weights))
 
 
 def run_inference(input_data: list[SimulationResult]) -> None:
