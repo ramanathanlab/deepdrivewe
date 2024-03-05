c29ed044e7f42ff439a5a25e4548064a0181f888
braceal <abrace05@gmail.com>
2024-03-04 19:12:21 -0600

    westpa workflow

--- westpa_colmena/westpa.py
+++ westpa_colmena/westpa.py
@@ -1,4 +1,5 @@
 """DeepDriveMD WESTPA example."""
+
 from __future__ import annotations
 
 import itertools
@@ -10,31 +11,28 @@ from functools import update_wrapper
 from pathlib import Path
 from typing import Any
 
-from colmena.models import Result
 from colmena.queue import ColmenaQueues
 from colmena.queue.python import PipeQueues
 from colmena.task_server import ParslTaskServer
-from colmena.thinker import agent
-from colmena.thinker import BaseThinker
-from colmena.thinker import event_responder
-from colmena.thinker import result_processor
 from proxystore.store import register_store
 from proxystore.store.file import FileStore
 
-from westpa_colmena.api import (  # InferenceCountDoneCallback,
-    DeepDriveMDSettings,
-)
-from westpa_colmena.api import (  # InferenceCountDoneCallback,
-    SimulationCountDoneCallback,
-)
-from westpa_colmena.api import (  # InferenceCountDoneCallback,
-    TimeoutDoneCallback,
-)
+from westpa_colmena.api import DeepDriveMDSettings
+from westpa_colmena.api import DeepDriveMDWorkflow
+from westpa_colmena.api import DoneCallback
+from westpa_colmena.api import SimulationCountDoneCallback
+from westpa_colmena.api import TimeoutDoneCallback
 from westpa_colmena.apps.amber_simulation import SimulationResult
+from westpa_colmena.apps.pcoord_inference import WeightedEnsemble
 from westpa_colmena.parsl import ComputeSettingsTypes
 
+# TODO: We need to send a random seed for amber simulations
+# TODO: It may good to implement the abstract batched simulation pattern
+#       within DeepDriveMD to do ablations and make the current westpa
+#       thinker implementation more elegant
 
-def run_simulation(
+
+def run_simulation(  # noqa: PLR0913
     output_dir: Path,
     amber_exe: Path,
     md_input_file: Path,
@@ -47,14 +45,6 @@ def run_simulation(
     from westpa_colmena.apps.amber_simulation import AmberSimulation
     from westpa_colmena.apps.amber_simulation import CppTrajAnalyzer
     from westpa_colmena.apps.amber_simulation import SimulationResult
-
-    # from proxystore.proxy import Proxy
-    # from proxystore.factory import SimpleFactory
-    # from westpa_colmena.apps.amber_simulation import SimulationResult as Result
-    # class SimulationResult(Result):
-    #     def __post_init__(self) -> None:
-    #         factory = SimpleFactory(self.coords)
-    #         self.coords = Proxy(factory)
 
     # First run the simulation
     simulation = AmberSimulation(
@@ -87,183 +77,102 @@ def run_simulation(
 
 
 def run_train(input_data: Any) -> None:
+    """Train a model on the input data."""
     ...
 
 
 def run_inference(input_data: list[SimulationResult]) -> None:
+    """Run inference on the input data."""
     ...
 
 
-class DeepDriveWESTPA(BaseThinker):  # type: ignore[misc]
-    def __init__(
+class DeepDriveWESTPA(DeepDriveMDWorkflow):
+    """A WESTPA thinker for DeepDriveMD."""
+
+    def __init__(  # noqa: PLR0913
         self,
         queue: ColmenaQueues,
         result_dir: Path,
         simulation_input_dir: Path,
         num_workers: int,
-        simulations_per_train: int,
+        done_callbacks: list[DoneCallback],
+        ensemble_members: int,
     ) -> None:
-        """
+        """Initialize the DeepDriveWESTPA thinker.
+
         Parameters
         ----------
-        queue:
+        queue: ColmenaQueues
             Queue used to communicate with the task server
-        result_dir:
+        result_dir: Path
             Directory in which to store outputs
         simulation_input_dir:
-            Directory with subdirectories each storing initial simulation start files.
-        num_workers:
+            Directory with subdirectories each storing initial simulation
+            start files.
+        num_workers: int
             Number of workers available for executing simulations, training,
             and inference tasks. One shared worker is reserved for training
             inference task, the rest (num_workers - 1) go to simulation.
+        done_callbacks: list[DoneCallback]
+            List of callbacks to determine when the thinker should stop.
+        ensemble_members: int
+            The number of simulations to start the weighted ensemble with.
         """
-        super().__init__(queue)
-
-        result_dir.mkdir(exist_ok=True)
-        self.result_dir = result_dir
-        self.num_workers = num_workers
-
-        # Collect initial simulation directories, assumes they are in nested subdirectories
-        self.simulation_input_dirs = itertools.cycle(
-            filter(lambda p: p.is_dir(), simulation_input_dir.glob('*')),
+        super().__init__(
+            queue,
+            result_dir,
+            simulation_input_dir,
+            num_workers,
+            done_callbacks,
+            async_simulation=False,
         )
 
-        # Keep track of the workflow state
-        self.simulations_completed = 0
+        # TODO: Check that this logic is correct
+        # Get the basis states by globbing the simulation input directories
+        basis_states: list[Path] = [  # type: ignore[misc]
+            p.glob('.ncrst')
+            for p in itertools.islice(
+                self.simulation_input_dirs,
+                ensemble_members,
+            )
+        ]
 
-        # Number of times a given task has been submitted
-        self.task_counter: defaultdict[str, int] = defaultdict(int)
-        self.done_callbacks = done_callbacks
+        # Initialize the weighted ensemble
+        self.weighted_ensemble = WeightedEnsemble(
+            basis_states=basis_states,
+            ensemble_members=ensemble_members,
+        )
 
-        # Communicate information between agents
-        self.simulation_govenor = Semaphore()
-        self.run_training = Event()
-        self.run_inference = Event()
-
+        # TODO: Update the inputs to support model weights, etc
         # Custom data structures
         self.train_input: list[SimulationResult] = []
         self.inference_input: list[SimulationResult] = []
-        # Always want to run inference on all the simulations in the batch
-        self.simulations_per_train = simulations_per_train
-        self.simulations_per_inference = self.num_workers - 1
 
-    def log_result(self, result: Result, topic: str) -> None:
-        """Write a JSON result per line of the output file."""
-        with open(self.result_dir / f'{topic}.json', 'a') as f:
-            print(result.json(exclude={'inputs', 'value'}), file=f)
+        self.current_idx = 0
+        self.ensemble_members = ensemble_members
 
-    def submit_task(self, topic: str, *inputs: Any) -> None:
-        self.queues.send_inputs(
-            *inputs,
-            method=f'run_{topic}',
-            topic=topic,
-            keep_inputs=False,
-        )
-        self.task_counter[topic] += 1
-
-    @agent  # type: ignore[misc]
-    def main_loop(self) -> None:
-        while not self.done.is_set():
-            for callback in self.done_callbacks:
-                if callback.workflow_finished(self):
-                    self.logger.info('Exiting DeepDriveMD')
-                    self.done.set()
-                    return
-            time.sleep(1)
-
-    @agent(startup=True)  # type: ignore[misc]
-    def start_simulations(self) -> None:
-        """Launch a first batch of simulations"""
-        # Save one worker for train/inference tasks
-        for _ in range(self.num_workers - 1):
-            self.simulate()
-
-    @result_processor(topic='simulation')  # type: ignore[misc]
-    def process_simulation_result(self, result: Result) -> None:
-        """Receive a training result and then submit new tasks
-
-        Will always submit a new simulation tasks and an inference or training
-        if the :meth:`handle_simulation_output` sets the appropriate flags.
-        """
-        # Log simulation job results
-        self.log_result(result, 'simulation')
-        if not result.success:
-            # TODO (wardlt): Should we submit a new simulation if one fails?
-            # (braceal): Yes, I think so. I think we can move this check to after submit_task()
-            self.logger.warning('Bad simulation result')
-            return
-
-        self.simulations_completed += 1
-
-        # NOTE: We had to remove the simulation submit here to implement simulation batching
-
-        # This function is running an implicit while-true loop
-        # we need to break out if the done flag has been sent,
-        # otherwise it will submit a new simulation.
-        if self.done.is_set():
-            return
-
-        # Result should be used to train the model and infer new restart points
-        self.handle_simulation_output(result.value)
-
-    # TODO (wardlt): We can have this event_responder allocate resources away from simulation if desired.
-    @event_responder(event_name='run_training')  # type: ignore[misc]
-    def perform_training(self) -> None:
-        self.logger.info('Started training process')
-
-        # Send in a training task
-        self.train()
-
-        # Wait for the result to complete
-        result: Result = self.queues.get_result(topic='train')
-        self.logger.info('Received training result')
-
-        self.log_result(result, 'train')
-        if not result.success:
-            self.logger.warning('Bad train result')
-            return
-
-        # Process the training output
-        self.handle_train_output(result.value)
-        self.logger.info('Training process is complete')
-
-    # TODO (wardlt): We can have this event_responder allocate resources away from simulation if desired.
-    @event_responder(event_name='run_inference')  # type: ignore[misc]
-    def perform_inference(self) -> None:
-        self.logger.info('Started inference process')
-
-        # Send in an inference task
-        self.inference()
-
-        # Wait for the result to complete
-        result: Result = self.queues.get_result(topic='inference')
-        self.logger.info('Received inference result')
-
-        self.log_result(result, 'inference')
-        if not result.success:
-            self.logger.warning('Bad inference result')
-            return
-
-        # Process the inference output
-        self.handle_inference_output(result.value)
-        self.logger.info('Inference process is complete')
+        # Make sure there has been at least one training task
+        # complete before running inference
+        self.model_weights_available = False
 
     def simulate(self) -> None:
         """Start a simulation task.
 
         Must call :meth:`submit_task` with ``topic='simulation'``
         """
-        ...
+        simulations = self.weighted_ensemble.current_iteration
+        self.submit_task('simulation', simulations[self.current_idx])
+        self.current_idx += 1
 
     def train(self) -> None:
         """Start a training task.
 
         Must call :meth:`submit_task` with ``topic='train'``
         """
-        ...
+        self.submit_task('train', self.train_input)
 
     def inference(self) -> None:
-        """Start an inference task
+        """Start an inference task.
 
         Must call a :meth:`submit_task` with ``topic='infer'``
         """
@@ -276,9 +185,11 @@ class DeepDriveWESTPA(BaseThinker):  # type: ignore[misc]
         self.logger.info('processed inference result')
 
     def handle_simulation_output(self, output: SimulationResult) -> None:
-        """Stores a simulation output in the training set and define new inference tasks
+        """Handle the output of a simulation.
 
-        Should call ``self.run_training.set()`` and/or ``self.run_inference.set()``
+        Stores a simulation output in the training set and define new
+        inference tasks Should call ``self.run_training.set()``
+        and/or ``self.run_inference.set()``.
 
         Parameters
         ----------
@@ -288,31 +199,48 @@ class DeepDriveWESTPA(BaseThinker):  # type: ignore[misc]
         # Collect simulation results
         self.train_input.append(output)
         self.inference_input.append(output)
-        # Since we are not clearing the train/inference inputs, the length will be the same
-        num_sims = len(self.train_input)
 
-        if num_sims and (num_sims % self.simulations_per_train == 0):
+        # Since we are not clearing the train/inference inputs, the
+        # length will be the same as the ensemble members
+        if self.ensemble_members % len(self.train_input) == 0:
             self.run_training.set()
-
-        # TODO: simulations_per_inference should be the same as the number of simulation workers
-        if num_sims and (num_sims % self.simulations_per_inference == 0):
             self.run_inference.set()
 
     def handle_train_output(self, output: Any) -> None:
-        """Use the output from a training run to update the model"""
-        ...
+        """Use the output from a training run to update the model."""
+        self.inference_input.model_weight_path = output.model_weight_path
+        self.model_weights_available = True
+        self.logger.info(
+            f'Updated model_weight_path to: {output.model_weight_path}',
+        )
 
     def handle_inference_output(self, output: Any) -> None:
-        """Use the output from an inference run to update the list of available simulations"""
+        """Handle the output of an inference run.
+
+        Use the output from an inference run to update the list of
+        available simulations.
+        """
+        # TODO: Figure out exactly what the output needs to contain
+        # In fact, the inference should just return the binning output
+        to_split, to_merge = self.binner.bin(output)
+
+        self.weighted_ensemble.advance_iteration(to_split, to_merge)
+
+        # Reset the current simulation index for the next iteration
+        self.current_idx = 0
+
         # Run the next batch simulations
-        for sim_input in output.simulation_inputs:
-            self.submit_task('simulation', sim_input)
+        for _ in range(self.num_workers - 1):
+            self.simulate()
 
 
 class ExperimentSettings(DeepDriveMDSettings):
     """Provide a YAML interface to configure the experiment."""
 
+    ensemble_members: int
+    """Number of simulations to start the weighted ensemble with."""
     compute_settings: ComputeSettingsTypes
+    """Settings for the compute environment."""
 
 
 if __name__ == '__main__':
@@ -360,13 +288,12 @@ if __name__ == '__main__':
         result_dir=cfg.run_dir / 'result',
         simulation_input_dir=cfg.simulation_input_dir,
         num_workers=cfg.num_workers,
-        simulations_per_train=cfg.simulations_per_train,
-        simulations_per_inference=cfg.simulations_per_inference,
         done_callbacks=[
             # InferenceCountDoneCallback(2),  # Testing
             SimulationCountDoneCallback(cfg.num_total_simulations),
             TimeoutDoneCallback(cfg.duration_sec),
         ],
+        ensemble_members=cfg.ensemble_members,
     )
     logging.info('Created the task server and task generator')
 
