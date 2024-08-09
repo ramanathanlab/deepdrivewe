894ae33083270ed88eb6f5a7c173c0e3c65c05bb
braceal <abrace05@gmail.com>
2024-08-09 13:41:25 -0500

    checkpoint

--- westpa_colmena/checkpoint.py
+++ westpa_colmena/checkpoint.py
@@ -33,7 +33,7 @@ class EnsembleCheckpointer:
 
         # Save the weighted ensemble to the checkpoint file
         with open(checkpoint_file, 'w') as fp:
-            fp.write(weighted_ensemble.json())
+            json.dump(weighted_ensemble.dict(), fp, indent=2)
 
         # Save the weighted ensemble to the HDF5 file
         self.h5file.append(
