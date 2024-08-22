6a5bb263dec3858d64390d5f3f1dc9d8319565f7
braceal <abrace05@gmail.com>
2024-08-22 12:47:13 -0500

    latent

--- deepdrivewe/examples/synd_ntl9_lof/inference.py
+++ deepdrivewe/examples/synd_ntl9_lof/inference.py
@@ -196,7 +196,9 @@ def run_inference(
         sim.append_pcoord(scores)
 
     # Log the latent space
-    pcoords = np.array(pcoords).reshape(-1, 1)
+    all_pcoords = [sim.metadata.pcoord for sim in input_data]
+    pcoords = np.array([p[0] for p in all_pcoords]).reshape(-1, 1)
+
     itetation = input_data[0].metadata.iteration_id
     output_dir = output_dir / f'{itetation:06d}'
     output_dir.mkdir(parents=True, exist_ok=True)
